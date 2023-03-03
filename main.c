ff6aa1f9127b60be1769ba00be2c15e2f9469b3f
ShobhitChaturvedi <113877844+ShobhitChaturvedi@users.noreply.github.com>
2023-03-03 09:36:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int x;
+ scanf("%d",&x);
+ switch(x){
+     case 1:
+     printf("Monday");
+     break;
+     case 2:
+     printf("Tuesday");
+     break; 
+     case 3:
+     printf("Wednesday");
+     break; 
+     case 4:
+     printf("thursday");
+     break; 
+     case 5:
+     printf("Friday");
+     break;
+     case 6:
+     printf("Saturday");
+     break;
+     case 7:
+     printf("Sunday");
+     break;
+     default:
+     printf("sahi no daalo");
+     
+ }
+
+    return 0;
+}
