a4efb786d71f5e3adf337790ec59addc3e6a4896
ShobhitChaturvedi <113877844+ShobhitChaturvedi@users.noreply.github.com>
2023-04-18 23:38:05 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int arr[50],i,N,l,s;
+
+     printf("Enter size of the array : ");
+    scanf("%d", &N);
+
+    
+    printf("Enter elements in array : ");
+    for(i=0; i<N; i++)
+    {
+        scanf("%d", & arr[i]);
+    }
+    l=s=arr[0];
+    for(i=1;i<N;i++)
+    {
+        if(arr[i]>l)
+        l=arr[i];
+        if(arr[i]<s)
+        s=arr[i];
+    }
+     printf("max: %d\n",l);
+     printf("min: %d",s);
+
+    return 0;
+}
+ 
